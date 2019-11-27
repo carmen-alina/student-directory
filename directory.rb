5c3d38ac71eac4a9bc534d7d554970117e07254e
Carmen-Alina <carmen.panu@gmail.com>
2019-11-27 15:45:22 +0000

    Added array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
+
+# let's put all students into an array
+students = [
+  "Dr Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddie Kruger",
+  "The joker",
+  "Jeffery Baratheon",
+  "Norman Bates"
+]
+#and then print them
 puts "The Students of Villains Academy"
 puts "------------------------------------"
-puts "Dr Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddie Kruger"
-puts "The joker"
-puts "Jeffery Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 # Finally we print the overall number of Students
-print "Overall we have "
-print 11
-puts " great students"
+puts "Overall we have #{students.count} great students"
