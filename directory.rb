bbbf1748471beaa3b8016f71dbf1b21c337ba174
Carmen-Alina <carmen.panu@gmail.com>
2019-11-27 16:18:36 +0000

    Added Hash's and cleaned code

--- directory.rb
+++ directory.rb
@@ -1,23 +1,36 @@
 
 # let's put all students into an array
 students = [
-  "Dr Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddie Kruger",
-  "The joker",
-  "Jeffery Baratheon",
-  "Norman Bates"
+  {name: "Dr Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Withc", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddie Kruger", cohort: :november},
+  {name: "The joker", cohort: :november},
+  {name: "Jeffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november},
 ]
 #and then print them
-puts "The Students of Villains Academy"
-puts "------------------------------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The Students of Villains Academy"
+  puts "------------------------------------"
 end
-# Finally we print the overall number of Students
-puts "Overall we have #{students.count} great students"
+
+def print_names(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+
+def print_footer(students)
+  puts "Overall we have #{students.count} great students"
+end
+
+print_header
+print_names(students)
+print_footer(students)
